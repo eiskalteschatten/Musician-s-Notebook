0a5fc1a8d52fda904410917bd5fa6e89267cc615
Alex <alex@alexseifert.com>
2014-05-08 21:36:31 +0200

    Textboxes added to XIB

---
+++ Musician's Notebook/SongArrayController.h
@@ -0,0 +1,13 @@
+//
+//  SongArrayController.h
+//  Musician's Notebook
+//
+//  Created by Alex Seifert on 5/6/14.
+//  Copyright (c) 2014 Alex Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SongArrayController : NSArrayController
+
+@end
---
+++ Musician's Notebook/SongArrayController.m
@@ -0,0 +1,22 @@
+//
+//  SongArrayController.m
+//  Musician's Notebook
+//
+//  Created by Alex Seifert on 5/6/14.
+//  Copyright (c) 2014 Alex Seifert. All rights reserved.
+//
+
+#import "SongArrayController.h"
+
+@implementation SongArrayController
+
+-(id)newObject {
+    id newObj = [super newObject];
+//    [newObj setValue:@"test" forKey:@"name"];
+    
+    
+    
+    return newObj;
+}
+
+@end
