b1bc98a1f62a55d4b0a3f38413a9490e63d1ab55
Alex <alex@alexseifert.com>
2014-06-08 20:40:33 +0200

    Added Notebook View Controller and cleaned up default toolbar

---
+++ Musician's Notebook/NotebooksViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotebooksViewController.h
+//  Musician's Notebook
+//
+//  Created by Alex Seifert on 6/8/14.
+//  Copyright (c) 2014 Alex Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NotebooksViewController : NSViewController
+
+@end
---
+++ Musician's Notebook/NotebooksViewController.m
@@ -0,0 +1,25 @@
+//
+//  NotebooksViewController.m
+//  Musician's Notebook
+//
+//  Created by Alex Seifert on 6/8/14.
+//  Copyright (c) 2014 Alex Seifert. All rights reserved.
+//
+
+#import "NotebooksViewController.h"
+
+@interface NotebooksViewController ()
+
+@end
+
+@implementation NotebooksViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
